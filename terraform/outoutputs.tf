6330820d956c65ef76c64a629cd567f6e74cfb82
Axongther <noa2006117@gmail.com>
2025-05-11 17:50:22 -0600

    Refactor output variables in Terraform configuration: update web server public and private IP outputs to handle multiple instances.

--- terraform/outoutputs.tf
+++ terraform/outoutputs.tf
@@ -1,4 +1,13 @@
-output "web_server_public_ip" {
-  description = "107.22.72.193"
-  value       = aws_instance.web_server.public_ip
+output "web_server_public_ips" {
+  description = "IPs públicas de los servidores web"
+  value       = aws_instance.web_server[*].public_ip
+}
+output "web_server_private_ips" {
+  description = "IPs privadas de los servidores web"
+  value       = aws_instance.web_server[*].private_ip
+}
+
+output "bastion_public_ip" {
+  description = "IP pública del bastión"
+  value       = aws_instance.bastion.public_ip
 }
